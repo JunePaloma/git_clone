9a8f88022081abae7cf9f85606bd8ab11954ce6b
JunePaloma <kborkoski@gmail.com>
2017-12-05 15:52:51 -0700

    fix typos - page behaves as expected

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     if user = GithubUser.from_omniauth(request.env["omniauth.auth"])
-       session[:github_user_id] = github_user.id
+       session[:user_id] = user.id
     end
        redirect_to root_path
   end
