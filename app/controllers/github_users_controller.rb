41409b5be75be4040f0903adf397d75b12870f34
JunePaloma <kborkoski@gmail.com>
2017-12-06 15:28:58 -0700

    Add initial API call to controller

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -1,5 +1,11 @@
 class GithubUsersController < ApplicationController
   def show
-    @user = User.
+    @conn =  Faraday.new(url: 'https://api.github.com') do |faraday|
+      faraday.params["access_token"] = "#{current_user.oauth_token}"
+      faraday.adapter Faraday.default_adapter
+    end
+  user_info = @conn.get("/users/#{@current_user.user_name}")
+    JSON.parse(user_info.body, symbolize_names: true )
   end
+
 end
--- spec/features/user_can_see_basic_info_spec.rb
+++ spec/features/user_can_see_basic_info_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 describe "As a logged-in user I can" do
   it "see basic information about my account" do
-
+    VCR.use_cassette("Github_service#basic_user_info") do
     visit "/"
     mock_auth_hash
     click_on("Sign in with Github")
     # expect(current_path).to eq(github_user_path)
 
-    # VCR.use_cassette("Github_service#basic_user_info")
-
     expect(page).to have_content("Stars 1")
     expect(page).to have_content("Followers 2")
     expect(page).to have_content("Following 4")
     expect(page).to have_content("Repositories 36")
-
+    end
   end
 end
