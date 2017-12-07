dfb8feaaa7c0b39624dea51ed8a19f5ec3b0926d
JunePaloma <kborkoski@gmail.com>
2017-12-06 17:46:57 -0700

    repositories call is working from the controller

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -4,8 +4,31 @@ class GithubUsersController < ApplicationController
       faraday.params["access_token"] = "#{current_user.oauth_token}"
       faraday.adapter Faraday.default_adapter
     end
-  user_info = @conn.get("/users/#{@current_user.user_name}")
-    JSON.parse(user_info.body, symbolize_names: true )
+  user_info = @conn.get("/users/#{@current_user.user_name}/repos")
+    results = JSON.parse(user_info.body, symbolize_names: true )
+    @repositories = results.map do |repo|
+    Repo.new(repo[:name], repo[:language])
+    end
   end
+end
 
+# class RepoOfRepos
+#   def initialize(repo_data)
+#     @data = repo_data
+#   end
+#
+# def make_individual_repos
+#   @data.map do |repo|
+#     SingleRepo.new(repo[:name], repo[:langage])
+#     end
+#   end
+# end
+
+class Repo
+  def initialize(name, language)
+  @name = name
+  @language = language
+  end
+  private
+  attr_reader :name, :language
 end
