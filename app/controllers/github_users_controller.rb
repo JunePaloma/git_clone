e31c513ca8463323126a883ffd4d5be2a3c1ae78
JunePaloma <kborkoski@gmail.com>
2017-12-06 18:51:50 -0700

    Followers call is working with class in the service

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -1,25 +1,6 @@
 class GithubUsersController < ApplicationController
   def show
     @repositories = GithubService.new(current_user).fetch_repos
-
-  #
-  #   @conn =  Faraday.new(url: 'https://api.github.com') do |faraday|
-  #     faraday.params["access_token"] = "#{current_user.oauth_token}"
-  #     faraday.adapter Faraday.default_adapter
-  #   end
-  # user_info = @conn.get("/users/#{@current_user.user_name}/repos")
-  #   results = JSON.parse(user_info.body, symbolize_names: true )
-  #   @repositories = results.map do |repo|
-  #   Repo.new(repo[:name], repo[:language], repo[:created_at])
-  #   end
+    @followers = GithubService.new(current_user).fetch_followers
   end
 end
-
-# class Repo
-#   def initialize(name, language, created_at)
-#   @name = name
-#   @language = language
-#   end
-#   private
-#   attr_reader :name, :language
-# end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -16,5 +16,24 @@ class GithubService
      repos
   end
 
+  def fetch_followers
+    follower_info = @conn.get("/users/#{@current_user.user_name}/followers")
+      results = JSON.parse(follower_info.body, symbolize_names: true )
+      followers = results.map do |follower|
+      User.new(follower[:login], follower[:url], follower[:id], follower[:avatar_url])
+    end
+    followers
+  end
+end
 
+class User
+  def initialize(login, url, id, avatar_url)
+    @login = login
+    @url = url
+    @id = id
+    @avatar_url = avatar_url
+  end
+
+  private
+  attr_reader :login, :url, :id, :avatar_url
 end
